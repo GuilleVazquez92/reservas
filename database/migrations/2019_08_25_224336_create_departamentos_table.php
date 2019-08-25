8505ab9fe7afb86ce25f56223a964934f4214a16
Guille <guillevaz92@gmail.com>
2019-08-25 18:59:59 -0400

    Agrega migrations de paises deptos y ciudades

---
+++ database/migrations/2019_08_25_224336_create_departamentos_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDepartamentosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('departamentos', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('idpais')->unsigned();
+            $table->string('descripcion',50);
+
+            $table->foreign('idpais')->references('id')->on('paises');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('departamentos');
+    }
+}
---
+++ database/migrations/2019_08_25_224401_create_ciudades_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCiudadesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ciudades', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('iddepartamento')->unsigned();
+            $table->string('descripcion',50);
+
+            $table->foreign('iddepartamento')->references('id')->on('departamentos');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ciudades');
+    }
+}
